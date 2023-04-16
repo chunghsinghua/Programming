5ebefe7ef583f7b90207f62e5109e1b31d7c0a6b
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2023-04-16 16:32:28 +0800

    Update APCS201703_秘密差-2.py

--- APCS201703_秘密差-2.py
+++ APCS201703_秘密差-2.py
@@ -1,8 +1,8 @@
-n = input()
-a = b = 0
-for i, s in enumerate(n):
-    if i & 1 == 0:
-        a += int(s)
+s = 0
+for i, x in enumerate(input()):
+    if i & 1:
+        s += int(x)
     else:
-        b += int(s)
-print(abs(a-b))
+        s -= int(x)
+
+print(abs(s))
