9012ed940d4547dd7438423c9586208abcf78ea5
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2023-05-13 23:28:36 +0800

    Create APCS201806_小崴的特殊編碼-2.py

---
+++ APCS201806_小崴的特殊編碼-2.py
@@ -0,0 +1,10 @@
+import sys
+dic={"0 1 0 1":'A','0 1 1 1':'B','0 0 1 0':'C','1 1 0 1':'D','1 0 0 0':'E','1 1 0 0':'F'}
+
+for i in sys.stdin:
+    n=int(i)
+    ans=[]
+    for _ in range(n):
+        s=sys.stdin.readline().strip()
+        ans.append(dic[s])
+    print(''.join(ans))
